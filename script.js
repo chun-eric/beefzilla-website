c690a037a52aca2c9edae6af6f534eb5c784e045
chun-eric <eric.chun88800888@gmail.com>
2023-07-18 15:09:43 +0900

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+// elements
+const reviews = document.querySelectorAll(".review-wrapper");
+
+let currentReview = [0, 2];
+
+let updateReviewSlider = (cards) => {
+  cards.forEach(index => {
+    reviews[index].classList.add("active")
+  });
+}
+
+setInterval(() => {
+  currentReview.forEach(index, i => {
+    reviews[index].classList.remove("active");
+
+    currentReview[i] = index >= reviews.length - 1 ? 0 : index + 1;
+  });
+
+  setTimeout(() => {
+    updateReviewSlider(currentReview);
+  }, 250);
+
+}, 5000)
+
+updateReviewSlider(currentReview);
