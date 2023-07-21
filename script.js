fe5d866e4301e1da0b625c6e4bd1d53e4b723aaf
chun-eric <eric.chun88800888@gmail.com>
2023-07-21 11:55:26 +0900

    finished home page html css and js

--- script.js
+++ script.js
@@ -3,25 +3,60 @@
 // elements
 const reviews = document.querySelectorAll(".review-wrapper");
 
+// cards review
 let currentReview = [0, 2];
 
+
+// function to add active class initally
 let updateReviewSlider = (cards) => {
-  cards.forEach(index => {
-    reviews[index].classList.add("active")
+  cards.forEach((card) => {
+    reviews[card].classList.add("active");
   });
-}
+};
 
+
+// setInterval function
 setInterval(() => {
-  currentReview.forEach(index, i => {
-    reviews[index].classList.remove("active");
+  // remove active class
+  currentReview.forEach((card, i) => {
+    reviews[card].classList.remove("active");
 
-    currentReview[i] = index >= reviews.length - 1 ? 0 : index + 1;
+    // updating the currentReview values by 1 based on reviews array length;
+    currentReview[i] = card >= reviews.length - 1 ? 0 : card + 1;
+    console.log(currentReview[i]);
   });
 
+
+  // setTimeout function
   setTimeout(() => {
     updateReviewSlider(currentReview);
-  }, 250);
-
-}, 5000)
+  }, 1250);
+}, 8000);
 
 updateReviewSlider(currentReview);
+
+
+
+// toggle FAQ sections
+const faqs = [...document.querySelectorAll(".faq")];
+
+faqs.map((faq) => {
+  let question = faq.querySelector(".question-box");
+  console.log(question);
+
+  question.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
+
+
+// function for dish slider
+let dishSlider = document.querySelector(".dish-slide");
+
+let rotationValue = 0;
+
+setInterval(() => {
+  rotationValue += 120;
+
+  dishSlider.style.transform = `translateY(-50%) rotate(${rotationValue}deg)`
+}, 7000)
